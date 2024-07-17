Fixed action encoding formula

python/agent.py
@@ -62,6 +62,7 @@ class PSAgent:
     def cross_wiring(self, a: int) -> tuple[int, int]:
         # a is the index of the action in the range [0..A-1], convert it 
         # to a pair of indices representing the edges to be cross-wired. 
-        i = np.floor(((2*self.A - 1) - np.sqrt((2*self.A-1)**2 - 8*a))//2, dtype=int)
-        j = (a - self.A*i + ((i+2)*(i+1))//2)
+        E = self.state_space_params[2]
+        i = np.floor(((2*E - 1) - np.sqrt((2*E-1)**2 - 8*a))//2).astype(int)
+        j = (a - E*i + ((i+2)*(i+1))//2)
         return i, j