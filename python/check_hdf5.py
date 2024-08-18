c20f2628a70c0fd2349764343d191fa24f73ff0f
BrunoCAF <brunocaf123@gmail.com>
2024-08-18 13:18:53 -0300

    Generic hdf5 verifier

--- python/check_hdf5.py
+++ python/check_hdf5.py
@@ -1,11 +1,12 @@
 import h5py
+import sys
 
-with h5py.File("exploration.hdf5", 'r') as f:
-    # f.visititems(lambda n, o: print(f"{n}: {o}"))
-    for grp in f.values():
-        print(grp.name)
-        for a in grp.attrs:
-            print(f"{grp}.{a} = {grp.attrs[a]}")
+with h5py.File(sys.argv[1], 'r') as f:
+    f.visititems(lambda n, o: print(f"{n}: {o}"))
+    # for grp in f.values():
+    #     print(grp.name)
+    #     for a in grp.attrs:
+    #         print(f"{grp}.{a} = {grp.attrs[a]}")
 
-        for ds in grp.values():
-            print(ds)
+    #     for ds in grp.values():
+    #         print(ds)
