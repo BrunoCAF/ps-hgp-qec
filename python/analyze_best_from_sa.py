ebde3e64fc6cb6dc1204f83eeea98af0fdebc83f
BrunoCAF <brunocaf123@gmail.com>
2024-09-01 02:05:21 -0300

    Reflect the change in temperature scheduling

--- python/analyze_best_from_sa.py
+++ python/analyze_best_from_sa.py
@@ -5,7 +5,7 @@ import argparse
 import numpy as np
 
 grpname = codes
-subgrpname = [f"{beta=:.0f}" for beta in [5, 10, 25, 50, 100, 200, 400]] + ['initial']
+subgrpname = [f"{beta=:.0f}" for beta in [1, 4, 7, 10]] + ['initial']
 p_vals = np.linspace(0.1, 0.5, 15)
 MC_budget = [int(5e5), int(1e5), int(1e5), int(5e4)]
 
@@ -20,14 +20,14 @@ if __name__ == '__main__':
     # Choose the code family and the beta parameter from which to pick the best (beta=0 for initial code)
     C, b = args.C, args.b
 
-    with h5py.File("best_from_sa.hdf5", "r") as f: 
+    with h5py.File("best_from_sadl.hdf5", "r") as f: 
         theta = from_edgelist(f[grpname[C]][subgrpname[b]]['theta'][()])
 
     results = MC_erasure_plog(num_trials=MC_budget[C], 
                               state=theta, 
                               p_vals=p_vals)
 
-    with h5py.File("best_from_sa.hdf5", "a") as f: 
+    with h5py.File("best_from_sadl.hdf5", "a") as f: 
         grp = f.require_group(grpname[C])
         subgrp = grp.require_group(subgrpname[b])
         subgrp.create_dataset("mean", data=results['mean'])
